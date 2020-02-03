384926053f1c7f3b15475814c0ed4adcebf5ab3b
Sureyya Celep <sureyyacelep@yahoo.com>
2020-02-03 03:12:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,7 +19,19 @@ def directors_totals(nds)
   #
   # Be sure to return the result at the end!
   pp nds
-  
+  director_index = 0
+  result = {}
+  while director_index < nds.length do
+    director_name = nds[director_index][:name]
+    result[director_name] = 0
+    movie_index = 0
+    while movie_index < nds[director_index][:movies].length do
+      result[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1
+    end
+    director_index += 1
+  end
+  result
   nil
 end
 
